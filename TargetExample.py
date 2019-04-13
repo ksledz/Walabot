32e3df70c3bb3efb4f7328d7a4b5fc963e5ca855
ksledz <kamila.ktos.sledz@gmail.com>
2019-04-13 15:32:56 +0200

    modified data harvesting (+ cleanup)

--- TargetExample.py
+++ TargetExample.py
@@ -1,23 +1,23 @@
-from __future__ import print_function # WalabotAPI works on both Python 2 an 3.
-from sys import platform
-from os import system
+from __future__ import print_function  # WalabotAPI works on both Python 2 an 3.
+from sys import platform, argv
+from os import system, path, makedirs
 from imp import load_source
-from os.path import join
 import time
 import csv
 import numpy as np
-import matplotlib.pyplot as plt
 
 if platform == 'win32':
-	modulePath = join('C:/', 'Program Files', 'Walabot', 'WalabotSDK',
-		'python', 'WalabotAPI.py')
+    modulePath = path.join('C:/', 'Program Files', 'Walabot', 'WalabotSDK', 'python',
+                           'WalabotAPI.py')
 elif platform.startswith('linux'):
-    modulePath = join('/usr', 'share', 'walabot', 'python', 'WalabotAPI.py')
+    modulePath = path.join('/usr', 'share', 'walabot', 'python', 'WalabotAPI.py')
 
 wlbt = load_source('WalabotAPI', modulePath)
 wlbt.Init()
 codeData = []
 codeDataTime = []
+
+
 def PrintSensorTargets(targets):
     system('cls' if platform == 'win32' else 'clear')
     if targets:
@@ -28,12 +28,14 @@ def PrintSensorTargets(targets):
     else:
         print('No Target Detected')
 
+
 def savesignalfile(a):
     with open("Data.csv", "w") as datafile:
         datafilewirte = csv.writer(datafile)
         for signalx in zip(a):
             # data = '{}\n'.format(signalx)
             datafilewirte.writerow(a)
+
 
 def PrintResults(targets):
     system('cls' if platform == 'win32' else 'clear')
@@ -45,9 +47,10 @@ def PrintResults(targets):
     else:
         print('No Target Detected')
 
+
 def SensorApp():
     # wlbt.SetArenaR - input parameters
-    minInCm, maxInCm, resInCm = 30, 200, 3
+    minInCm, maxInCm, resInCm = 30, 500, 3
     # wlbt.SetArenaTheta - input parameters
     minIndegrees, maxIndegrees, resIndegrees = -15, 15, 3
     # wlbt.SetArenaPhi - input parameters
@@ -56,8 +59,10 @@ def SensorApp():
     mtiMode = False
     # Configure Walabot database install location (for windows)
     wlbt.SetSettingsFolder()
+
     # 1) Connect : Establish communication with walabot.
     wlbt.ConnectAny()
+
     # 2) Configure: Set scan profile and arena
     # Set Profile - to Sensor.
     wlbt.SetProfile(wlbt.PROF_SENSOR)
@@ -73,19 +78,24 @@ def SensorApp():
     wlbt.SetDynamicImageFilter(filterType)
     # wlbt.SetThreshold(100)
     incrementer = 0
+
     # 3) Start: Start the system in preparation for scanning.
     wlbt.Start()
     t0 = time.time()
-    if not mtiMode: # if MTI mode is not set - start calibrartion
+    if not mtiMode:  # if MTI mode is not set - start calibrartion
         # calibrates scanning to ignore or reduce the signals
         wlbt.StartCalibration()
         while wlbt.GetStatus()[0] == wlbt.STATUS_CALIBRATING:
             wlbt.Trigger()
-    while int(time.time()-t0) <=20:
+    start_time = time.perf_counter()
+    mtimes = []
+    while int(time.time() - t0) <= 30:
         appStatus, calibrationProcess = wlbt.GetStatus()
+
         # 5) Trigger: Scan(sense) according to profile and record signals
         # to be available for processing and retrieval.
         wlbt.Trigger()
+
         # 6) Get action: retrieve the last completed triggered recording
         targets = wlbt.GetSensorTargets()
         rasterImage, _, _, sliceDepth, power = wlbt.GetRawImageSlice()
@@ -95,19 +105,35 @@ def SensorApp():
         I = Antenna[11]
         print(Antenna[11])
         signal = wlbt.GetSignal(I)
+
+        timedelta = (time.perf_counter() - start_time) * 1000
+        if mtimes:
+            print(timedelta - mtimes[-1])
+        mtimes.append(timedelta)
+
         print(len(signal[0]))
         codeData.append(signal[0])
         codeDataTime.append(signal[1])
-        incrementer+=1
+        incrementer += 1
+
     # 7) Stop and Disconnect.
     wlbt.Stop()
     wlbt.Disconnect()
-    print ('pulse',incrementer)
+    print('pulse', incrementer)
     print('Terminate successfully')
     print(len(codeData))
-    # savesignalfile(codeData)
-    np.savetxt('Data/Data.out', codeData, delimiter=',', fmt='%f')
-    np.savetxt('Data/DataTime.out', codeDataTime, delimiter=',', fmt='%1.9e')
+
+    testname = ""
+    if len(argv) > 0:
+        testname = argv[1]
+
+    if not path.exists('Data'):
+        makedirs('Data')
+
+    np.savetxt('Data/Data' + testname + '.out', codeData, delimiter=',', fmt='%f')
+    np.savetxt('Data/DataTime' + testname + '.out', codeDataTime, delimiter=',', fmt='%1.9e')
+    np.savetxt('Data/DataTimes' + testname + '.out', mtimes, delimiter=',', fmt='%1.9e')
+
 
 if __name__ == '__main__':
     SensorApp()
